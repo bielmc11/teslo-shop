73cfe8fbd1ebda1836bfce181096a6212b94c24b
bielmc11 <bielmarticr@gmail.com>
2024-11-13 13:48:27 +0100

    pagination done

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -4,9 +4,25 @@ import prisma from "@/lib/prisma";
 
 //Aqui haré server accions, recuerda que se puede hacer lo mimso haciendo endPoints y lueg en vez de server accions las funciiones que lo llamen como se hace tradicionalmente
 
-export const getPaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+  take?: number;
+  page?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 10,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
+  if (isNaN(Number(take))) take = 10;
+  if (take < 1) take = 1;
+
   try {
     const products = await prisma.product.findMany({
+      take,
+      skip: take * (page - 1),
       include: {
         images: {
           take: 2,
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,16 +1,25 @@
 import { getPaginatedProductsWithImages } from "@/actions/product/product-pagination";
 import { ProductGrid, Title } from "@/components/inedx";
-import { initialData } from "@/seed/seed";
+//import { initialData } from "@/seed/seed";
 
 //const products = initialData.products
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
-export default async function Home() {
-  const { products } = await getPaginatedProductsWithImages();
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const { products } = await getPaginatedProductsWithImages({ page });
+
+  console.log("mis productos son: ", products[0]);
 
   return (
     <div>
       <Title title="tienda" subtitle="Todos los productos" classname="mb-2" />
-       <ProductGrid products={products} /> 
+      <ProductGrid products={products} />
     </div>
   );
 }
