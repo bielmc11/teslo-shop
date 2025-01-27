77289a90c4fecedfed4d06af73f58c825f09c106
bielmc11 <bielmarticr@gmail.com>
2025-01-27 12:49:58 +0100

    updated product done

--- src/actions/admin/products/create-update-product.ts
+++ src/actions/admin/products/create-update-product.ts
@@ -1,11 +1,14 @@
 'use server'
 
+import prisma from "@/lib/prisma";
+import { Product, Size } from "@prisma/client";
 import { z } from "zod"
 
 const dataSchema = z.object({
     id: z.string().uuid().optional(),
     title: z.string().min(3).max(255),
     description: z.string().min(3).max(255),
+    slug: z.string().min(3).max(255),
 
     price: z.coerce.number().min(0).transform((val) => val.toFixed(2)),
     inStock: z.coerce.number().min(0).transform((val) => val.toFixed(2)),
@@ -24,7 +27,6 @@ export const createUpdateProduct = async (formData: FormData) => {
 
 
 
-    //? Como saco toda la info de un formdata en un solo objeto 
     const data = Object.fromEntries(formData);
 
     console.log(data)
@@ -34,11 +36,75 @@ export const createUpdateProduct = async (formData: FormData) => {
 
 
     if(!parsedData.success){
+        console.log('error parsedData no funciono')
         return {
             ok: false,
             error: parsedData.error
         }
     }
+
+    
+    const product = parsedData.data;
+    product.slug = product.slug.toLowerCase()
+
+    const {id, inStock, price, ...rest} = product
+
+
+
+    const prismaTransaction = await prisma.$transaction( async (tx) => {
+
+        let product : Product
+        const tagsArray = rest.tags.split(",").map(tag => tag.trim().toLowerCase())
+
+        
+        const processedList = rest.sizes.map(item => item.replace(/[\[\]"]/g, ''))
+        
+        
+
+        if(id){ //si tengo id actualizar
+
+
+            
+
+            product = await tx.product.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    ...rest,
+                    sizes: processedList as Size[],
+                    tags: {
+                        set: tagsArray
+                    },
+                    inStock: Number(inStock),
+                    price: Number(price)
+                }
+            })
+
+            console.log({updatedProduct: product})
+
+
+        }else{ // si no tengo id crear
+
+            return {
+                
+            }
+
+        }
+
+
+
+
+        
+    })
+
+    //TODO si todo sale bien revlidate path
+
+    return {
+        ok: true,
+    }
+
+
 
 
 
